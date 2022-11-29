45d4999ab25e67487db9b1b0d609b07803fae6cc
Aldanis Vigo <aldanisvigo@gmail.com>
2022-11-29 13:52:21 -0600

    added comments

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -5,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ContactList {
+public class ContactList implements  Serializable {
     private List<Contact> contactList;
     private Scanner inputScanner;
 
     public ContactList(Scanner inputScanner){
         this.inputScanner = inputScanner;
+        readAllContent();
     }
 
     public Contact readInContact(){
@@ -26,30 +28,54 @@ public class ContactList {
         return newContact;
     }
 
-
-    public void writeAllContacts(List<String> contacts) {
+    public void writeAllContacts() {
         try {
+            //Get a path to the file
+            Path filepath = Paths.get("data", "./contacts.txt");
 
-            Path filepath = Paths.get("data", "contacts.txt");
-            Files.write(filepath, contacts);
+            //If the file does not exist create it
+            if(Files.notExists(filepath)) {
+                Files.createFile(filepath);
+            }
+
+            //Get an output stream to the file
+            FileOutputStream stream = new FileOutputStream(filepath.toString());
+
+            //Create an object output stream so we can write the contact objects
+            ObjectOutputStream outStream = new ObjectOutputStream(stream);
+
+            //Iterate through the contacts and save the objects to the file
+            outStream.writeObject(this);
 
         } catch (Exception e) {
-
             System.out.println(e.getMessage());
-
         }
     }
 
-    public List<String> readAllContent() {
+    public void readAllContent() {
         try {
+            //Get the path to the file
             Path contactPath = Paths.get("data", "contacts.txt");
-            List<String> contactList = Files.readAllLines(contactPath);
 
-            return contactList;
+            //If the file does not exist, create a file
+            if(Files.notExists(contactPath)){
+                Files.createFile(contactPath);
+            }
+
+            //Get a file input stream using the path
+            FileInputStream inStream = new FileInputStream(contactPath.toString());
+
+            //Get an object input stream
+            ObjectInputStream objInStream = new ObjectInputStream(inStream);
+
+            //Create a temporary ContactList object from the contents of the file
+            ContactList tmp = (ContactList) objInStream.readObject();
+
+            //Steal its contact list and write it into this.contactList
+            this.contactList = tmp.contactList;
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return new ArrayList<>();
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
-
-
-
-
-
-
     public static void main(String[] args) {
-
+        Scanner myScanner = new Scanner(System.in);
+        ContactList myList = new ContactList(myScanner);
     }
-
 }
