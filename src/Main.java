667cac49cf9e149641c9e0b90d1c4ac9b2c7f304
John-Michael Krsak <jmkrsak@gmail.com>
2022-11-29 10:29:35 -0500

    added class to read all content

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
 
+    public void readAllContent() {
+
+        Path groceriesPath = Paths.get("data", "groceries.txt");
+        List<String> groceryList = Files.readAllLines(groceriesPath);
+
+        for (int i = 0; i < groceryList.size(); i += 1) {
+            System.out.println((i + 1) + ": " + groceryList.get(i));
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("yo mama");
+
     }
 
 }
